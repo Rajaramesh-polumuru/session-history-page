697d358c2d71bbec9857c97228b9b86581bba4f7
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 02:12:25 +0530

    Refactor Content container component and code clean up

--- components/session-history/ContentContainer.tsx
+++ components/session-history/ContentContainer.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import ActivityGraph from "./ActivityGraph";
+import { FC } from "react";
+import SessionActivityGraph from "./SessionActivityGraph";
 import SessionHistoryTable from "./SessionHistoryTable";
 import useMutateSessionHistoryData from "@/hooks/useMutateSessionHistoryData";
 import { SessionHistoryResponse } from "@/pages/api/v1/types";
@@ -8,6 +8,7 @@ import SummaryCards from "./SummaryCards";
 interface ContentContainerProps {
   sessionHistoryResponse: SessionHistoryResponse;
 }
+
 const ContentContainer: FC<ContentContainerProps> = ({
   sessionHistoryResponse,
 }) => {
@@ -17,9 +18,15 @@ const ContentContainer: FC<ContentContainerProps> = ({
   return (
     <div className="flex flex-col gap-6 w-full h-full overflow-auto">
       <div className="text-2xl font-tomorrow">Session History</div>
-      <SummaryCards data={sessionHistoryResponse?.summaryData} />
-      <ActivityGraph data={activityGraphData} />
-      <SessionHistoryTable data={sessionHistoryTableData} />
+      {sessionHistoryResponse?.summaryData && (
+        <SummaryCards data={sessionHistoryResponse.summaryData} />
+      )}
+      {activityGraphData.length > 0 && (
+        <SessionActivityGraph data={activityGraphData} />
+      )}
+      {sessionHistoryTableData.length > 0 && (
+        <SessionHistoryTable data={sessionHistoryTableData} />
+      )}
     </div>
   );
 };
