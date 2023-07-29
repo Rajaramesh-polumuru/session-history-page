8061857817f2310d5f9551ed1aa84a85ec89a995
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 02:13:24 +0530

    Refactor and clean up Header component

--- components/session-history/Header.tsx
+++ components/session-history/Header.tsx
@@ -1,43 +1,31 @@
 import Image from "next/image";
-import React from "react";
 import arrowLeftIcon from "@/public/assets/arrow-left.svg";
+import logo from "@/public/assets/logo.svg";
 
 const Header = () => {
   return (
-    <div className="flex align-middle justify-between w-full  px-6 py-4">
-      <div className="flex align-middle justify-between w-full  p-5 border-b-2 border-borderColor ">
-        <div className="flex gap-3 items-center">
-          <button className="border-none cursor-pointer">
-            <Image
-              src={arrowLeftIcon}
-              alt="arrow-left"
-              width={32}
-              height={32}
-            />
-          </button>
-          <div className="text-2xl font-bold font-tomorrow ">Instance Name</div>
-        </div>
-        <div className="grid grid-cols-2 gap-4 items-center ">
-          <div className="grid gap-4 border-r-2 border-slate-500 pr-5">
-            <div className="text-2xl font-bold w-max font-tomorrow">
-              Node Moniker
-            </div>
-            <div className="text-md">sent1jqd5s...axu6c</div>
-          </div>
-          <button className="bg-customGreen hover:bg-customGreen text-white font-bold py-2 px-4 rounded h-fit w-full">
-            Start
-          </button>
-        </div>
-        <div className="flex  items-center">
-          <Image
-            src="/assets/logo.svg"
-            alt="arrow-left"
-            width={150}
-            height={40}
-          />
-        </div>
+    <header className="flex justify-between px-6 py-4 w-full">
+      <div className="flex items-center gap-3">
+        <button type="button" className="border-none cursor-pointer">
+          <Image src={arrowLeftIcon} alt="arrow-left" width={32} height={32} />
+        </button>
+        <h2 className="text-2xl font-bold font-tomorrow">Instance Name</h2>
       </div>
-    </div>
+      <nav className="grid grid-cols-2 gap-4 items-center">
+        <div className="grid gap-4 border-r-2 border-slate-500 pr-5">
+          <h3 className="text-2xl font-bold w-max font-tomorrow">
+            Node Moniker
+          </h3>
+          <p className="text-md">sent1jqd5s...axu6c</p>
+        </div>
+        <button className="bg-customGreen hover:bg-customGreen text-white font-bold py-2 px-4 rounded h-fit w-full">
+          Start
+        </button>
+      </nav>
+      <div className="flex items-center">
+        <Image src={logo} alt="logo" width={150} height={40} />
+      </div>
+    </header>
   );
 };
 
