03bc689f53c3eedf87c291bbfb6eb6ec49a5434b
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 00:42:15 +0530

    Update header styles

--- components/session-history/Header.tsx
+++ components/session-history/Header.tsx
@@ -15,23 +15,25 @@ const Header = () => {
               height={32}
             />
           </button>
-          <div className="text-2xl font-bold ">Instance Name</div>
+          <div className="text-xl font-bold ">Instance Name</div>
         </div>
         <div className="grid grid-cols-2 gap-4 items-center ">
           <div className="grid gap-4 border-r-2 border-slate-500 pr-5">
-            <div className="text-2xl font-bold w-max">Node Moniker</div>
-            <div className="">sent1jqd5s...axu6c</div>
+            <div className="text-xl font-bold w-max">Node Moniker</div>
+            <div className="text-md">sent1jqd5s...axu6c</div>
           </div>
           <button className="bg-customGreen hover:bg-customGreen text-white font-bold py-2 px-4 rounded h-fit w-full">
             Start
           </button>
         </div>
-        <Image
-          src="/assets/logo.svg"
-          alt="arrow-left"
-          width={225}
-          height={68}
-        />
+        <div className="flex  items-center">
+          <Image
+            src="/assets/logo.svg"
+            alt="arrow-left"
+            width={150}
+            height={40}
+          />
+        </div>
       </div>
     </div>
   );
