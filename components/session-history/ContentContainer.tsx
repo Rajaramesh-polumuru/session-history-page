1c3eba4288fc6d7dd39225940e76d74736b6d42e
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-29 21:46:41 +0530

    Add Content container styles

--- components/session-history/ContentContainer.tsx
+++ components/session-history/ContentContainer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { FC } from "react";
+import ActivityGraph from "./ActivityGraph";
+import QuickCards from "./QuickCards";
+import SessionHistoryTable from "./SessionHistoryTable";
+import useMutateSessionHistoryData from "@/hooks/useMutateSessionHistoryData";
+import { SessionHistoryResponse } from "@/pages/api/v1/types";
 
-const ContentContainer = () => {
-  return (
-    <div>ContentContainer</div>
-  )
+interface ContentContainerProps {
+  sessionHistoryResponse: SessionHistoryResponse;
 }
+const ContentContainer: FC<ContentContainerProps> = ({
+  sessionHistoryResponse,
+}) => {
+  const { activityGraphData, sessionHistoryTableData } =
+    useMutateSessionHistoryData(sessionHistoryResponse?.sessionHistory);
 
-export default ContentContainer
+  return (
+    <div className="flex flex-col gap-6 w-full h-full overflow-auto">
+      <div className="text-2xl ">Session History</div>
+      <QuickCards data={sessionHistoryResponse?.summaryData} />
+      <ActivityGraph data={activityGraphData} />
+      <SessionHistoryTable data={sessionHistoryTableData}/>
+    </div>
+  );
+};
+
+export default ContentContainer;
