3080f6f77d6de61a8a033f01c25d36d3f6f23b52
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 02:15:42 +0530

    Add types and refactor mutation hook

--- hooks/useMutateSessionHistoryData.ts
+++ hooks/useMutateSessionHistoryData.ts
@@ -1,34 +1,56 @@
 import { SessionData } from "@/pages/api/v1/types";
 import { useEffect, useState } from "react";
 
-export default function useMutateSessionHistoryData(
-  sessionHistory: SessionData[]
-) {
-  const [activityGraphData, setActivityGraphData] = useState<any>([]);
-  const [sessionHistoryTableData, setSessionHistoryTableData] = useState<any>(
-    []
-  );
-  useEffect(() => {
-    const newActivityGraphData = sessionHistory?.map((session) => {
-      return {
-        date: session.date,
-        duration: Number(session.duration.split(" ")[0] ?? 0),
-        startTime: session.startTime, 
-      };
-    });
-    setActivityGraphData(newActivityGraphData);
+interface ActivityGraphData {
+  date: string;
+  duration: number;
+  startTime: string;
+}
 
-    const newSessionHistoryTableData = sessionHistory?.map((session) => {
-      return {
-        startTime: session.startTime,
-        date: session.date,
-        duration: session.duration,
-        nodeUsed: session.nodeUsed,
-        dataConsumed: session.dataConsumed,
-        location: session.location,
-      };
-    });
-    setSessionHistoryTableData(newSessionHistoryTableData);
+interface SessionHistoryTableData {
+  startTime: string;
+  date: string;
+  duration: string;
+  nodeUsed: string;
+  dataConsumed: string;
+  location: string;
+}
+
+export default function useMutateSessionHistoryData(
+  sessionHistory: SessionData[] | undefined
+) {
+  const [activityGraphData, setActivityGraphData] = useState<
+    ActivityGraphData[]
+  >([]);
+  const [sessionHistoryTableData, setSessionHistoryTableData] = useState<
+    SessionHistoryTableData[]
+  >([]);
+
+  useEffect(() => {
+    const newActivityGraphData = sessionHistory?.map(
+      ({ date, duration, startTime }) => {
+        return {
+          date,
+          duration: Number(duration.split(" ")[0]) || 0,
+          startTime,
+        };
+      }
+    );
+    setActivityGraphData(newActivityGraphData || []);
+
+    const newSessionHistoryTableData = sessionHistory?.map(
+      ({ date, duration, startTime, nodeUsed, dataConsumed, location }) => {
+        return {
+          date,
+          duration,
+          startTime,
+          nodeUsed,
+          dataConsumed: dataConsumed,
+          location,
+        };
+      }
+    );
+    setSessionHistoryTableData(newSessionHistoryTableData || []);
   }, [sessionHistory]);
 
   return { activityGraphData, sessionHistoryTableData };
