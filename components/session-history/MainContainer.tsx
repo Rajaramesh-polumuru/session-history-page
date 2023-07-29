f1ea13d40f68214e947027b8fc42d26f0a0b4393
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-29 21:47:22 +0530

    Add Main container styles

--- components/session-history/MainContainer.tsx
+++ components/session-history/MainContainer.tsx
@@ -4,7 +4,11 @@ interface MainContainerProps {
   children: React.ReactNode;
 }
 const MainContainer: FC<MainContainerProps> = ({ children }) => {
-  return <div className=" w-full h-full flex ">{children}</div>;
+  return (
+    <div className=" w-full h-full flex px-6 py-4 gap-5 overflow-hidden">
+      {children}
+    </div>
+  );
 };
 
 export default MainContainer;
