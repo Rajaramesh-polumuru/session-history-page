a77f36358b895f0437f228220c080cfeaaebdc24
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 02:16:15 +0530

    Add types and refactor useSessionHistory hook

--- hooks/session-history/useSessionHistory.ts
+++ hooks/session-history/useSessionHistory.ts
@@ -1,6 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, QueryFunction } from "@tanstack/react-query";
+import { SessionHistoryResponse } from "@/pages/api/v1/types";
 
-const fetchSessionHistory = async (userId: number) => {
+const fetchSessionHistory: QueryFunction<SessionHistoryResponse> = async () => {
+  const userId = 1;
   const response = await fetch(`/api/v1/session-history?userId=${userId}`);
   if (!response.ok) {
     throw new Error("Failed to fetch session history data");
@@ -9,9 +11,7 @@ const fetchSessionHistory = async (userId: number) => {
 };
 
 const useSessionHistory = (userId: number) => {
-  return useQuery(["sessionHistory", userId], () =>
-    fetchSessionHistory(userId)
-  );
+  return useQuery(["sessionHistory", userId], fetchSessionHistory);
 };
 
 export default useSessionHistory;
