b4ea8d2021238d5eaa68eac33e916b08b2ed9b90
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 00:12:17 +0530

    Update Left nav styles and add data

--- components/session-history/LeftNav.tsx
+++ components/session-history/LeftNav.tsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { FC } from "react";
 import Image from "next/image";
 import nodeOverviewIcon from "@/public/assets/node-overview.svg";
 import caretRightIcon from "@/public/assets/caret-right.svg";
 
-const LeftNav = () => {
+interface LeftNavProps {
+  sessionData: {
+    title: string;
+    value: string;
+  }[];
+  navItems: {
+    name: string;
+    icon: string;
+    key: string;
+  }[];
+  activeKey: string;
+}
+
+const LeftNav: FC<LeftNavProps> = ({ navItems, sessionData, activeKey }) => {
   return (
-    <div className="w-1/3 bg-customGray rounded-xl p-5">
-      <div className="w-full flex gap-4 border-b-2 border-borderColor p-5 ">
-        <div className=" text-textGray w-max">IP Address </div>
-        <div>: </div>
-        <div>136.251.15.107</div>
-      </div>
-      <div className="my-4 grid gap-4 h-full overflow-auto w-full ">
-        <div className="flex items-center h-fit p-4 bg-cardBackground rounded-xl justify-between gap-4 border-borderColor border-2 active:border-accent hover:border-accent cursor-pointer">
-          <Image
-            src={nodeOverviewIcon}
-            alt="arrow-left"
-            width={24}
-            height={24}
-          />
-          <div className="w-full text-textGray">Node Overview</div>
-          <Image src={caretRightIcon} alt="arrow-left" width={10} height={10} />
+    <div className="w-1/3 bg-customGray rounded-xl p-5 min-w-max h-full overflow-hidden flex flex-col">
+      {sessionData?.map((item) => (
+        <div
+          className="w-full flex gap-4 border-b-2 border-borderColor p-5"
+          key={item.title}
+        >
+          <div className="text-textGray w-max">{item.title}</div>
+          <div>:</div>
+          <div>{item.value}</div>
+        </div>
+      ))}
+      <div className="w-full flex gap-4 border-b-2 border-borderColor my-5 py-4">
+        <div className="grid gap-4 h-fit w-full overflow-auto ">
+          {navItems?.map((item) => (
+            <div
+              className={`flex items-center p-4 bg-cardBackground rounded-xl justify-between gap-4 ${
+                activeKey === item.key ? "border-accent" : "border-borderColor"
+              } border-2 hover:border-accent cursor-pointer h-fit`}
+              key={item.name}
+            >
+              <Image src={item.icon} alt="arrow-left" width={24} height={24} />
+              <div className="w-full text-textGray text-ellipsis overflow-hidden whitespace-nowrap">
+                {item.name}
+              </div>
+              <Image
+                src={caretRightIcon}
+                alt="arrow-left"
+                width={10}
+                height={10}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- pages/session-history/index.tsx
+++ pages/session-history/index.tsx
@@ -4,11 +4,15 @@ import LeftNav from "@/components/session-history/LeftNav";
 import MainContainer from "@/components/session-history/MainContainer";
 import React from "react";
 import backgroundImage from "@/public/assets/images/background.png";
-import {
-  QueryClient,
-} from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import useSessionHistory from "@/hooks/session-history/useSessionHistory";
 import Image from "next/image";
+import nodeOverviewIcon from "@/public/assets/left-nav-icons/node-overview.svg";
+import dvpnEarningsIcon from "@/public/assets/left-nav-icons/dvpn-earnings.svg";
+import sessionHistoryIcon from "@/public/assets/left-nav-icons/session-history.svg";
+import subscriptionHistoryIcon from "@/public/assets/left-nav-icons/subscription-history.svg";
+import bandwidthUsageIcon from "@/public/assets/left-nav-icons/bandwidth-usage.svg";
+import editNodeConfigurationIcon from "@/public/assets/left-nav-icons/edit-node-configuration.svg";
 
 const SessionHistory = () => {
   // For now, we'll just hardcode the userId
@@ -16,6 +20,52 @@ const SessionHistory = () => {
   const queryClient = new QueryClient();
   const { data, isLoading, error } = useSessionHistory(userId);
 
+  const NavItemsList = [
+    {
+      name: "Node Overview",
+      icon: nodeOverviewIcon,
+      key: "node-overview",
+    },
+    {
+      name: "DVPN Earnings",
+      icon: dvpnEarningsIcon,
+      key: "dvpn-earnings",
+    },
+    {
+      name: "Session History",
+      icon: sessionHistoryIcon,
+      key: "session-history",
+    },
+    {
+      name: "Subscription History",
+      icon: subscriptionHistoryIcon,
+      key: "subscription-history",
+    },
+    {
+      name: "Bandwidth Usage",
+      icon: bandwidthUsageIcon,
+      key: "bandwidth-usage",
+    },
+    {
+      name: "Edit Node Configuration",
+      icon: editNodeConfigurationIcon,
+      key: "edit-node-configuration",
+    },
+  ];
+  //should be fetched from the api
+  const sessionData = [
+    {
+      title: "IP Address",
+      value: "136.251.15.107",
+    },
+    {
+      title: "Country",
+      value: "Germany",
+    },
+  ];
+
+  //should be dynamic
+  const activeKey = "session-history";
   return (
     <div className="h-screen flex w-screen  items-center flex-col">
       <Image
@@ -26,7 +76,11 @@ const SessionHistory = () => {
       />
       <Header />
       <MainContainer>
-        <LeftNav />
+        <LeftNav
+          navItems={NavItemsList}
+          sessionData={sessionData}
+          activeKey={activeKey}
+        />
         <ContentContainer sessionHistoryResponse={data} />
       </MainContainer>
     </div>
