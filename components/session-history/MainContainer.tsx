619be81f704ce478f2bb3366c763db43d61edc03
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-30 02:14:12 +0530

    Clean up the code

--- components/session-history/MainContainer.tsx
+++ components/session-history/MainContainer.tsx
@@ -1,11 +1,12 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
 interface MainContainerProps {
   children: React.ReactNode;
 }
+
 const MainContainer: FC<MainContainerProps> = ({ children }) => {
   return (
-    <div className=" w-full h-full flex px-6 py-4 gap-5 overflow-hidden">
+    <div className="w-full h-full flex px-6 py-4 gap-5 overflow-hidden">
       {children}
     </div>
   );
